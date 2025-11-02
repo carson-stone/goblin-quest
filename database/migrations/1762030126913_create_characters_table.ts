31f412f3e578348007109704d235e7293df2b306
Carson Stone <carson.stone123@gmail.com>
2025-11-02 16:29:49 -0500

    add name column to characters table

--- database/migrations/1762030126913_create_characters_table.ts
+++ database/migrations/1762030126913_create_characters_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name', 40).notNullable()
       table.integer('user_id').unsigned().notNullable().references('id').inTable('users')
       table
         .integer('character_class_id')
